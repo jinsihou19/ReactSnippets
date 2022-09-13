45bc05b13698c2417b212eb5b109bb98f73008e4
vito <vito@finereport.com>
2022-09-13 12:32:53 +0800

    migrate: Migrate deprecated APIs to support hot deployment

--- src/com/jinsihou/react/snippets/ReactTemplateProvider.java
+++ src/com/jinsihou/react/snippets/ReactTemplateProvider.java
@@ -1,22 +1,22 @@
 package com.jinsihou.react.snippets;
 
 
-import com.intellij.codeInsight.template.impl.DefaultLiveTemplatesProvider;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.codeInsight.template.TemplateActionContext;
+import com.intellij.codeInsight.template.TemplateContextType;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author jinsihou
  * @date 2017/10/18
  */
-public class ReactTemplateProvider implements DefaultLiveTemplatesProvider {
-    @Override
-    public String[] getDefaultLiveTemplateFiles() {
-        return new String[]{"liveTemplates/React"};
+public class ReactTemplateProvider extends TemplateContextType {
+    protected ReactTemplateProvider() {
+        super("REACT", "React");
     }
 
-    @Nullable
     @Override
-    public String[] getHiddenLiveTemplateFiles() {
-        return new String[0];
+    public boolean isInContext(@NotNull TemplateActionContext templateActionContext) {
+        String name = templateActionContext.getFile().getName();
+        return name.endsWith(".js") || name.endsWith(".jsx") || name.endsWith(".html") || name.endsWith(".ts");
     }
 }
